e060918096963bbc38192ae18a4b78d4fa44b29d
Anshu <acharya.anshu@gmail.com>
2025-09-17 14:26:12 -0500

    Fix formatting in Constants.java and remove unused pivotCommand method in SalvageManipulator.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -51,7 +51,7 @@ public final class Constants {
         public static final int intakeMotorID = 6;
         public static final int pivotMotorID = 7;
 
-        //Salvage Motor IDs
+        // Salvage Motor IDs
         public static final int salvagePivotMotorID = 8;
     }
 }
--- src/main/java/frc/robot/subsystems/SalvageManipulator.java
+++ src/main/java/frc/robot/subsystems/SalvageManipulator.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.MotorIDs;
 
 public class SalvageManipulator {
@@ -9,9 +8,5 @@ public class SalvageManipulator {
 
     public SalvageManipulator() {
         pivotMotor = new TalonSRX(MotorIDs.salvagePivotMotorID); // Example motor ID
-    }
-
-    public Command pivotCommand(double speed) {
-        return null;
     }
 }
