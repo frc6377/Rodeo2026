1657a98a193280dd9e5769e847bf08b1d6d7d22e
Anshu <acharya.anshu@gmail.com>
2025-09-24 20:42:49 -0500

    Implement pivot control in ScrapIntake subsystem with angle retrieval and position command

--- src/main/java/frc/robot/subsystems/Scrap/ScrapIntake.java
+++ src/main/java/frc/robot/subsystems/Scrap/ScrapIntake.java
@@ -3,6 +3,9 @@ package frc.robot.subsystems.scrap;
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix6.hardware.CANcoder;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.MotorIDs;
 
@@ -15,11 +18,29 @@ public class ScrapIntake extends SubsystemBase {
     public ScrapIntake() {
         intakeMotor = new TalonSRX(MotorIDs.intakeMotorID);
         pivotMotor = new TalonSRX(MotorIDs.pivotMotorID);
+        pivotMotor.config_kP(0, 0.1);
+        pivotMotor.config_kI(0, 0.0);
+        pivotMotor.config_kD(0, 0.0);
+
         pivotEncoder = new CANcoder(MotorIDs.pivotEncoderID);
+
+
     }
 
-    public void goToPivotPosition(double position) {
-        // Code to move pivot to a specific position
+    public Angle getPivotAngle() {
+        return pivotEncoder.getPosition().getValue();
+    }
+
+    public Command goToPivotPosition(double position) {
+        return Commands.run(
+                () -> {
+                    pivotMotor.set(TalonSRXControlMode.Position, position);
+                },
+                this);
+    }
+
+    public boolean atSetpoint(Angle setpoint) {
+        return pivotEncoder.getPosition().equals(setpoint);
     }
 
     public void setIntakeSpeed(double speed) {
