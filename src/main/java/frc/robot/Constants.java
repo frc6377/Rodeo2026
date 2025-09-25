f00807c6de63d073169748558c955e175abb9c94
Anshu <acharya.anshu@gmail.com>
2025-09-24 19:45:35 -0500

    Add pivot encoder ID to Constants and implement ScrapIntake subsystem with encoder support

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -50,6 +50,7 @@ public final class Constants {
         // Scrap Motors IDs
         public static final int intakeMotorID = 6;
         public static final int pivotMotorID = 7;
+        public static final int pivotEncoderID = 9;
 
         // Salvage Motor IDs
         public static final int salvagePivotMotorID = 8;
--- src/main/java/frc/robot/subsystems/Scrap/ScrapIntake.java
+++ src/main/java/frc/robot/subsystems/Scrap/ScrapIntake.java
@@ -1,16 +1,21 @@
-package frc.robot.subsystems.Scrap;
+package frc.robot.subsystems.scrap;
 
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix6.hardware.CANcoder;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.MotorIDs;
 
-public class ScrapIntake {
+public class ScrapIntake extends SubsystemBase {
     private TalonSRX intakeMotor;
     private TalonSRX pivotMotor;
+
+    private CANcoder pivotEncoder;
 
     public ScrapIntake() {
         intakeMotor = new TalonSRX(MotorIDs.intakeMotorID);
         pivotMotor = new TalonSRX(MotorIDs.pivotMotorID);
+        pivotEncoder = new CANcoder(MotorIDs.pivotEncoderID);
     }
 
     public void goToPivotPosition(double position) {
