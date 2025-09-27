724a7e54024caad5339e43c0b521c814124375ea
Vaedz7 <75952433+Vaedz7@users.noreply.github.com>
2025-09-27 11:35:20 -0500

    Remove old Salvage files

--- src/main/java/frc/robot/subsystems/salvage/Salvage.java
+++ src/main/java/frc/robot/subsystems/salvage/Salvage.java
@@ -12,8 +12,8 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.MotorIDs;
-import frc.robot.Constants.SensorIDs;;
-import frc.robot.Constants.PivotConstants;;;
+import frc.robot.Constants.SensorIDs;
+import frc.robot.Constants.PivotConstants;
 
 
 public class Salvage extends SubsystemBase {
