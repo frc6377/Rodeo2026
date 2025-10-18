47cfe249f787bc474b0b3720f3c37ece26f83e14
Vaedz7 <75952433+Vaedz7@users.noreply.github.com>
2025-10-18 15:36:59 -0500

    Add drop to forward auton.

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -62,7 +62,10 @@ public final class Autos {
             Salvage salvageSubsystem,
             ScrapIntake scrapIntakeSubsystem,
             ScrapShooter scrapShooterSubsystem) {
-        return Commands.sequence(driveSubsystem.setForwardCommand(1.5, 0.45)).withName("LeaveDockAuto");
+        return Commands.sequence(
+            dropToZero(salvageSubsystem),
+            driveSubsystem.setForwardCommand(1.5, 0.45))
+        .withName("LeaveDockAuto");
     }
 
     public static Command ShootScrapAuto(
