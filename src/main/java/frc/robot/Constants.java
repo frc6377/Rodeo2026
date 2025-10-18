d97bdeb43565989542bbd9cd1f85480a4c06813c
Anshu <acharya.anshu@gmail.com>
2025-10-18 16:43:40 -0500

    constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -30,7 +30,7 @@ import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
  * the value of "simMode" to switch between "sim" (physics sim) and "replay" (log replay from a file).
  */
 public final class Constants {
-    public static final boolean isJared = false;
+    public static final boolean isJared = true;
 
     public static final boolean tuningMode = false;
     public static final boolean useKeyboard = false;
